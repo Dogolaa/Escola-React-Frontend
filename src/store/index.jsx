6f9d3093ef4b6c27ca68db15d898d0dec4699da8
Lucas Dogo <lucasdogo01@gmail.com>
2025-02-15 20:12:11 -0300

    feat: Split Actions and Reducers

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,23 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-
-const initialState = {
-  botaoClicado: false,
-};
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'BOTAO_CLICADO': {
-      const newState = { ...state };
-      newState.botaoClicado = !newState.botaoClicado;
-      return newState;
-    }
-    default:
-      return state;
-  }
-};
+import rootReducer from './modules/rootReducer';
 
 const store = configureStore({
-  reducer,
+  reducer: rootReducer, // CORREÇÃO: precisa estar dentro de "reducer"
 });
 
 export default store;
