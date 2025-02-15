07418d811d21841ac5ea2fb65e3cdb8135545f6e
Lucas Dogo <lucasdogo01@gmail.com>
2025-02-15 20:53:35 -0300

    feat: Redux Saga

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,8 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: rootReducer, // CORREÇÃO: precisa estar dentro de "reducer"
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
