03c49d1a068678ba10fb5ca712ba96b5f773e417
Lucas Dogo <lucasdogo01@gmail.com>
2025-02-18 21:18:57 -0300

    feat: Now Users photos are displayed

--- src/pages/Aluno/index.jsx
+++ src/pages/Aluno/index.jsx
@@ -4,10 +4,11 @@ import { isFloat, isInt, isEmail } from 'validator';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 import { useDispatch } from 'react-redux';
+import { FaEdit, FaUserCircle } from 'react-icons/fa';
 
 import { Container } from '../../styles/GlobalStyles';
-import { useNavigate, useParams } from 'react-router-dom';
-import { Form } from './styled';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Form, ProfilePicture, Title } from './styled';
 import Loading from '../../components/Loading';
 import axios from '../../services/axios';
 import * as actions from '../../store/modules/auth/actions';
@@ -20,6 +21,7 @@ export default function Aluno() {
   const [idade, setIdade] = useState('');
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
+  const [foto, setFoto] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
 
@@ -33,6 +35,8 @@ export default function Aluno() {
         setIsLoading(true);
         const { data } = await axios.get(`/alunos/${id}`);
         const Foto = get(data, 'Fotos[0].url', '');
+
+        setFoto(Foto);
 
         setNome(data.nome);
         setSobrenome(data.sobrenome);
@@ -139,8 +143,16 @@ export default function Aluno() {
   return (
     <Container>
       <Loading isLoading={isLoading} />
-      <h1>{id ? 'Editar aluno' : 'Novo Aluno'}</h1>
+      <Title>{id ? 'Editar aluno' : 'Novo Aluno'}</Title>
 
+      {id && (
+        <ProfilePicture>
+          {foto ? <img src={foto} alt={nome} /> : <FaUserCircle size={180} />}
+          <Link to={`/fotos/${id}`}>
+            <FaEdit size={24} />
+          </Link>
+        </ProfilePicture>
+      )}
       <Form onSubmit={handleSubmit}>
         <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} placeholder="Nome" />
         <input type="text" value={sobrenome} onChange={(e) => setSobrenome(e.target.value)} placeholder="Sobrenome" />
